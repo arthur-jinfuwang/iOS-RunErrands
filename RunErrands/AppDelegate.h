cd0b35b991ef5093a37364f3cf6d7b111bb5a158
Jinfu Wang <ginfu.wang@gmail.com>
2015-07-20 20:23:07 +0800

    Initial Commit

---
+++ RunErrands/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RunErrands
+//
+//  Created by Jinfu Wang on 2015/7/20.
+//  Copyright (c) 2015年 Jinfu Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RunErrands/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RunErrands
+//
+//  Created by Jinfu Wang on 2015/7/20.
+//  Copyright (c) 2015年 Jinfu Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
