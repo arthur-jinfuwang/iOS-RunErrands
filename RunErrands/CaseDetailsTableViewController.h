552a23fc82c223d9a1ec6617d5677d60e592e328
Jinfu Wang <ginfu.wang@gmail.com>
2015-07-27 17:47:50 +0800

    Make up the menus: case details and resume.

---
+++ RunErrands/CaseDetailsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CaseDetailsTableViewController.h
+//  RunErrands
+//
+//  Created by Jinfu Wang on 2015/7/27.
+//  Copyright (c) 2015年 Jinfu Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CaseDetailsTableViewController : UITableViewController
+
+@end
