9fe170cb758d5d37dd08aae9bf9a30691e55fc78
Jinfu Wang <ginfu.wang@gmail.com>
2015-07-29 09:58:55 +0800

    1. New add the SetLocationViewController menu.

---
+++ RunErrands/SetLocationViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetLocationViewController.h
+//  RunErrands
+//
+//  Created by Jinfu Wang on 2015/7/29.
+//  Copyright (c) 2015年 Jinfu Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetLocationViewController : UIViewController
+
+@end
