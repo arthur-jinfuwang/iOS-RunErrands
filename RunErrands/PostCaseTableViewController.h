a5504d231bc3674c8337da26e504e7da396331bb
Jinfu Wang <ginfu.wang@gmail.com>
2015-08-05 11:06:46 +0800

    1. Change the post case setting menu from view control to table view control.

---
+++ RunErrands/PostCaseTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  PostCaseTableViewController.h
+//  RunErrands
+//
+//  Created by Jinfu Wang on 2015/8/5.
+//  Copyright (c) 2015年 Jinfu Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SlideNavigationController.h"
+
+@interface PostCaseTableViewController : UITableViewController <SlideNavigationControllerDelegate>
+
+@end
---
+++ RunErrands/PostCaseTableViewController.m
@@ -0,0 +1,108 @@
+//
+//  PostCaseTableViewController.m
+//  RunErrands
+//
+//  Created by Jinfu Wang on 2015/8/5.
+//  Copyright (c) 2015年 Jinfu Wang. All rights reserved.
+//
+
+#import "PostCaseTableViewController.h"
+
+@interface PostCaseTableViewController ()
+{
+    NSMutableArray *listDetails;
+}
+
+@end
+
+@implementation PostCaseTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    listDetails = [[NSMutableArray alloc]initWithObjects:
+                   @"標題",@"工作起始時間",@"設定工作時間",@"設定酬勞",@"設定地點",@"撰寫工作內容",
+                   @"撰寫聯絡方式",nil];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return listDetails.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"postCase" forIndexPath:indexPath];
+    
+    // Configure the cell...
+     cell.textLabel.text = listDetails[indexPath.row];
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
